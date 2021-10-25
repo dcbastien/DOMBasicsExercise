ba2e9b3ff642cc92b36c689e4524495ec71b662d
dcbastien <ddcbastien@gmail.com>
2021-10-25 14:29:48 -0500

    Created initial files index.html app.js style.css to complete exercise

---
+++ app.js
@@ -0,0 +1,26 @@
+//  //Using getElementById, select the div with the info for Portillo's inside of it and set it to a const variable called portillos.
+// 2. Using getElementsByTagName, select all the images and set them to a const variable called images
+// 3. Using getElementsByClassName, select all the elements with the class of "center" and set them to a const variable called centered
+// 4. Using querySelector, select the first div with the info for Gino's East inside of it and set it to a const variable called ginosEast.
+// 5. Using querySelectorAll, select all the p elements and set them to a const variable called pTags
+// 6a. Select the h1 (You can use whichever selector you want) and set it to a const variable called h1.
+// 6b. Use inner text to change the text to say "Matt's Favorite Places To Eat In Sweet Home Chicago!"
+// 7a. Select the div with the id of "others" and set it to a const variable called others.
+// 7b. Use innerHTML to change the HTML to an h3 with the text of "Other favorites" inside of it
+// 8a. Select the a tag at the bottom of the HTML file and set it to a const variable called aTag.
+// 8b. Change the href attribute to the following link "https://www.choosechicago.com/articles/food-drink/5-classic-chicago-foods/"
+// 9. Using classList, ADD both the "background" and "text-color" classes to the h1 (NOTE: You can use the h1 variable you created earlier)
+// 10. Using classList, REMOVE the "background" class from the h1 (NOTE: You can use the h1 variable you created earlier)
+// 11a. Using createElement, create an h4 element and set it to a const variable called h4
+// 11b. Using innerText add the following text "CHICAGO: A great place to eat!"
+// 11c. Using prepend, add the h4 to the start of the webpage/document (NOTE: You need to select the body element before you do this)
+// 12a. Using createElement, create an h5 element and set it to a const variable called h5
+// 12b. Using innerText add the following text "See you in the Windy City Sometime!"
+// 12c. Using insertAdjacentElement, add the h5 after the closing tag of the a tag (AKA After the closing tag for the link) on the webpage/document (NOTE: You can use the aTag variable you created earlier)
+// 13. Using remove, remove the "Jay's Beef" li (NOTE: You will need to select the li first)
+
+// BONUS 
+// 14a. Select all the divs and set it to a const variable called divs.
+// 14b. Using classList, TOGGLE the "background" class on for all the divs (NOTE: You will need a loop to do this)
+// 15a. Create an array called hungry with the following string values "Thanks", "A", "Lot", "Now", "I", "Am", "Hungry"
+// 15b. Using a loop, add each item from the hungry array as span elements/tags to the bottom of the document/webpage
